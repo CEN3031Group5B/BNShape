e514fec1aa2d56fc4fbfb6a1a822dd95ae8e398d
Yu Yuan <yuzyuan93@gmail.com>
2015-10-12 17:39:56 -0400

    Redesigned the login and signup page
    Signup page does not have drop down menu working for country and
    region/state

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -10,14 +10,14 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.menu = Menus.getMenu('topbar');
 
     // Toggle the menu items
-    $scope.isCollapsed = false;
+    $scope.isCollapsed = true;
     $scope.toggleCollapsibleMenu = function () {
       $scope.isCollapsed = !$scope.isCollapsed;
     };
 
     // Collapsing the menu after navigation
     $scope.$on('$stateChangeSuccess', function () {
-      $scope.isCollapsed = false;
+      $scope.isCollapsed = true;
     });
   }
 ]);
