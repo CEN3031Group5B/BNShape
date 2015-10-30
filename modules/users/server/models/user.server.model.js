a7bb750871e220b388001b90a4693bcf017757fe
Yu Yuan <yuzyuan93@gmail.com>
2015-10-30 16:25:48 -0400

    Added new schemas into the database

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -61,7 +61,64 @@ var UserSchema = new Schema({
   },
   password: {
     type: String,
-    default: ''
+    required: 'Please fill in a password'
+  },
+  telephone: {
+    type: String, 
+    required: 'Please fill in a telephone number'
+  },
+  fax: {
+    type: String,
+    //default: ''
+  },
+  company: {
+    type: String,
+    //default: ''
+  },
+  companyId: {
+    type: String,
+    //default: ''
+  },
+  address1: {
+    type: String,
+    required: 'Please fill in an address'
+    //default: ''
+  },
+  address2: {
+    type: String,
+    //default: ''
+  },
+  city: {
+    type: String,
+    //default: ''
+  },
+  postCode: {
+    type: String,
+    //default: ''
+  },
+  country: {
+    type: String,
+    required: 'Please select a country'
+    //default: ''
+  },
+  region: {
+    type: String,
+    required: 'Please select a region/state'
+    //default: ''
+  },
+  
+  /*
+  yes: {
+    type: String,
+    //default: ''
+  }, */
+  radio: {
+    type: String,
+    //default: ''
+  }, 
+  policy: {
+    type: String,
+    required: 'You must agree to create a new account'
   },
   salt: {
     type: String
@@ -98,6 +155,7 @@ var UserSchema = new Schema({
   resetPasswordExpires: {
     type: Date
   }
+
 });
 
 /**
